999f8cecb0c9cc62ee785bca79bfc0dd75cc1e39
Demetri <dnekos2462@gmail.com>
2020-10-04 16:36:11 -0400

    Revert "Test fix using https://sigitov.de/how-to-work-with-an-sqlite-database-in-a-unity-2018-application/"
    This reverts commit 5d691aaf3a10b2e524c44f093bd43e16ec035540.

--- Assets/Scripts/Query_Test.cs
+++ Assets/Scripts/Query_Test.cs
@@ -1,20 +1,56 @@
 ﻿using UnityEngine;
-using System.Data.SQLite;
-public class DBConnector : MonoBehaviour
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class Query_Test : MonoBehaviour
 {
+
+    // Use this for initialization
     void Start()
     {
-        SQLiteConnection connection =
-                         new SQLiteConnection(@"Data Source=F:\mydb.db;Version=3;");
-        connection.Open();
-        SQLiteCommand command = connection.CreateCommand();
-        command.CommandType = System.Data.CommandType.Text;
-        command.CommandText = "CREATE TABLE IF NOT EXISTS 'highscores' ( " +
-                          "  'id' INTEGER PRIMARY KEY, " +
-                          "  'name' TEXT NOT NULL, " +
-                          "  'score' INTEGER NOT NULL" +
-                          ");";
-        command.ExecuteNonQuery();
-        connection.Close();
+
+        // Create database
+        string connection = "URI=file:" + Application.dataPath + "/" + "MegMart.db";
+        Debug.Log(connection);
+        // Open connection
+        IDbConnection dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+        /*
+        // Create table
+        IDbCommand dbcmd;
+        dbcmd = dbcon.CreateCommand();
+        string q_createTable = "CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, val INTEGER )";
+
+        dbcmd.CommandText = q_createTable;
+        dbcmd.ExecuteReader();
+
+        // Insert values in table
+        IDbCommand cmnd = dbcon.CreateCommand();
+        cmnd.CommandText = "INSERT INTO my_table (id, val) VALUES (0, 5)";
+        cmnd.ExecuteNonQuery();
+        */
+        // Read and print all values in table
+        IDbCommand cmnd_read = dbcon.CreateCommand();
+        IDataReader reader;
+        string query = "SELECT * FROM ItemDrops";
+        cmnd_read.CommandText = query;
+        reader = cmnd_read.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Debug.Log("id: " + reader[0].ToString());
+            Debug.Log("val: " + reader[1].ToString());
+        }
+
+        // Close connection
+        dbcon.Close();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 }
